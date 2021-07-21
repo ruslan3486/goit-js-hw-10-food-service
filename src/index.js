faaa4dc72e94c4c1e80639ff3f633fd5f0ee05ff
Ruslan Kuzma <kuzrus31@gmail.com>
2021-07-21 21:04:22 +0300

    homework 10

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import './sass/main.scss';
+import menu from './tamplates/menu.hbs';
+import menuMarkup from './menu.json'
+
+// Рендерим разметки меню
+
+const menuItem = document.querySelector('.js-menu');
+const menuMarkupItem = createMenuDisnes(menuMarkup);
+
+menuItem.insertAdjacentHTML('afterbegin', menuMarkupItem );
+function createMenuDisnes(menuMarkup) {
+    return menuMarkup.map(menu).join('');
+  
+
+}
+
+
+//Для удобства хранения списка тем используем такое перечисление
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+// Создаем обект body и input
+const refs ={
+ body: document.querySelector('body'),
+ addTheme: document.querySelector('#theme-switch-toggle')
+}
+ 
+refs.addTheme.addEventListener('change', onClickTheme);
+
+// Добавляем функционал изменения темы при нажатии (событие change) на чекбокс #theme-switch-toggle в тулбаре.
+
+function onClickTheme() {
+    const check = refs.addTheme.checked;
+
+    if (check) {
+        refs.body.classList.add(Theme.DARK)
+        refs.body.classList.remove(Theme.LIGHT)
+
+        // Используем localStorage для сохранения темы при перезагрузки.сохраняем данные пару ключ/значение
+        localStorage.setItem('background', Theme.DARK);
+        
+        
+    } else {
+        refs.body.classList.add(Theme.LIGHT);
+        refs.body.classList.remove(Theme.DARK);
+        localStorage.removeItem('theme');
+ // Используем localStorage для сохранения темы при перезагрузки.сохраняем данные пару ключ/значение
+         localStorage.setItem('background', Theme.LIGHT)
+    }
+
+}
+
+// Используем localStorage для сохранения темы при перезагрузки.Получаем данние значения ключа
+refs.addTheme.addEventListener("change", saveTheme)
+    
+    function saveTheme() {
+    
+    let background = localStorage.getItem("background");
+
+    if (background) {
+        
+        // refs.body.className += background;
+        
+        //ставим свойство checked в положении true штобы при перезагрузки не сдвигалса ползунок
+
+        refs.addTheme.checked = true;
+    } 
+
+}
